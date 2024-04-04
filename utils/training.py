c14ed8be1d80340edff53dc586ababbde1636c90
arka-windows11 <arkad.sh@yandex.ru>
2024-04-04 17:28:09 +0700

    Added support of val and test datasets. Added save best model.

--- utils/training.py
+++ utils/training.py
@@ -21,29 +21,40 @@ class Trainer:
     def __init__(
         self,
         train_dataset: torch.utils.data.Dataset,
+        val_dataset: torch.utils.data.Dataset,
         test_dataset: torch.utils.data.Dataset,
-        batch_size: int = 32,
+        batch_size: int,
         device: str | None = None,
     ):
         """Helper class for training different models.
 
         Args:
-            train_dataset (torch.utils.data.Dataset): train dataset
-            test_dataset (torch.utils.data.Dataset): test dataset
-            batch_size (int, optional): batch size. Defaults to 32.
+            train_dataset (torch.utils.data.Dataset): train dataset.
+            val_dataset (torch.utils.data.Dataset): val dataset.
+            test_dataset (torch.utils.data.Dataset): test dataset.
+            batch_size (int): batch size.
             device (str | None, optional): name of device where calculations will happen. Defaults to None.
 
         Attributes:
             train_dataloader (DataLoader): dataloader generator for training the model.
-            test_dataloader (DataLoader): dataloader generator for evaluating the model.
-            targets (tensor): tensor of target classes of testing dataset. Needed to plot confusion matrix.
+            val_dataloader (DataLoader | None): dataloader generator for validating the model.
+            test_dataloader (DataLoader | None): dataloader generator for evaluating the model.
+            targets (tensor): tensor of target classes of test or val dataset. Needed to plot confusion matrix.
             classes (list): list of classes.
             device (str): 'cpu' or 'cuda' device where training will happen.
         """
-        self.train_dataloader = DataLoader(train_dataset, batch_size, shuffle=True)
-        self.test_dataloader = DataLoader(test_dataset, batch_size, shuffle=False)
 
-        self.targets = test_dataset.targets
+        self.val_dataloader = None
+        self.test_dataloader = None
+
+        self.train_dataloader = DataLoader(train_dataset, batch_size, shuffle=True)
+        if val_dataset is not None:
+            self.val_dataloader = DataLoader(val_dataset, batch_size, shuffle=False)
+            self.targets = val_dataset.targets
+        if test_dataset is not None:
+            self.test_dataloader = DataLoader(test_dataset, batch_size, shuffle=False)
+            self.targets = test_dataset.targets
+
         self.classes = train_dataset.classes
 
         if device is None:
@@ -58,18 +69,18 @@ class Trainer:
         optimizer: torch.optim.Optimizer,
         accuracy_fn: torchmetrics.Accuracy,
         writer: Writer,
-        epochs: int = 10,
+        epochs: int,
     ):
         """Main training loop. Train and test model, record loss and accuracy,
         print progress to the command line.
 
         Args:
-            model (torch.nn.Module): model for training
-            loss_fn (torch.nn.Module): loss function
-            optimizer (torch.optim.Optimizer): optimizer
-            accuracy_fn (torchmetrics.Accuracy): accuracy fuction
-            writer (Writer): object to track progress and save it to csv
-            epochs (int): number of epochs for training
+            model (torch.nn.Module): model for training.
+            loss_fn (torch.nn.Module): loss function.
+            optimizer (torch.optim.Optimizer): optimizer.
+            accuracy_fn (torchmetrics.Accuracy): accuracy fuction.
+            writer (Writer): object to track progress and save it to csv.
+            epochs (int): number of epochs for training.
         """
         for epoch in range(epochs):
 
@@ -81,25 +92,50 @@ class Trainer:
             # run training
             train_loss, train_acc = self.train_step(model, self.train_dataloader, loss_fn, optimizer, accuracy_fn, bar)
 
-            # reinitialize progress bar for testing
-            bar = tqdm(ncols=120, desc=f"Epoch {epoch+1}/{epochs}", leave=False)
+            if self.val_dataloader is not None:
+                # reinitialize progress bar for validating
+                bar = tqdm(ncols=120, desc=f"Epoch {epoch+1}/{epochs}", leave=False)
 
-            # run testing
-            test_loss, test_acc = self.test_step(model, self.test_dataloader, loss_fn, accuracy_fn, bar)
+                # run validating
+                val_loss, val_acc = self.test_step(model, self.val_dataloader, loss_fn, accuracy_fn, bar)
 
-            print(
-                f"Epoch {epoch+1}/{epochs} | time: {time.time()-start:.2f}sec. | "
-                f"train_loss: {train_loss:.4f} | train_acc: {train_acc:.4f} | "
-                f"test_loss: {test_loss:.4f} | test_acc: {test_acc:.4f}"
-            )
+                print(
+                    f"Epoch {epoch+1}/{epochs} | time: {time.time()-start:.2f}sec. | "
+                    f"train_loss: {train_loss:.4f} | train_acc: {train_acc:.4f} | "
+                    f"val_loss: {val_loss:.4f} | val_acc: {val_acc:.4f}"
+                )
 
-            # add losses and metrics to the writer
-            writer.update(train_loss=train_loss, train_accuracy=train_acc, test_loss=test_loss, test_accuracy=test_acc)
+                # add losses and metrics to the writer
+                writer.update(train_loss=train_loss, train_accuracy=train_acc, val_loss=val_loss, val_accuracy=val_acc)
+
+            else:
+                print(
+                    f"Epoch {epoch+1}/{epochs} | time: {time.time()-start:.2f}sec. | "
+                    f"train_loss: {train_loss:.4f} | train_acc: {train_acc:.4f}"
+                )
+
+                # add losses and metrics to the writer
+                writer.update(train_loss=train_loss, train_accuracy=train_acc)
+
+            # save trained model
+            torch.save(model, os.path.join(writer.project_path, "last.pt"))
+            if writer.is_best("val_loss"):
+                torch.save(model, os.path.join(writer.project_path, "best.pt"))
 
         # save training info
         writer.plot_pairs()
         writer.save_to_csv()
-        self.plot_confusion_matrix(model, writer.project_path)
+
+        # try to load best model if there is any
+        best_path = os.path.join(writer.project_path, "best.pt")
+        if os.path.exists(best_path):
+            print(f"Evaluating {best_path}")
+            model = torch.load(best_path)
+
+        if self.test_dataloader is not None:
+            self.plot_confusion_matrix(model, writer.project_path, self.test_dataloader)
+        elif self.val_dataloader is not None:
+            self.plot_confusion_matrix(model, writer.project_path, self.val_dataloader)
         print(f"Info saved to {writer.project_path}")
 
     def train_step(
@@ -114,15 +150,15 @@ class Trainer:
         """Main loop of the training step per one epoch.
 
         Args:
-            model (torch.nn.Module): model to train
-            train_dataloader (torch.utils.data.DataLoader): data that will be fed to the model
-            loss_fn (torch.nn.Module): loss function
-            optimizer (torch.optim.Optimizer): optimizer
-            accuracy_fn (torchmetrics.Accuracy): accuracy function
-            bar (tqdm): tqdm bar object for printing progress
+            model (torch.nn.Module): model to train.
+            train_dataloader (torch.utils.data.DataLoader): data that will be fed to the model.
+            loss_fn (torch.nn.Module): loss function.
+            optimizer (torch.optim.Optimizer): optimizer.
+            accuracy_fn (torchmetrics.Accuracy): accuracy function.
+            bar (tqdm): tqdm bar object for printing progress.
 
         Returns:
-            tuple(float, float): train loss and train accuracy for printing and recording to csv
+            tuple(float, float): train loss and train accuracy for printing and recording to csv.
         """
         train_loss, train_acc = 0, 0
         model.to(self.device)
@@ -167,17 +203,17 @@ class Trainer:
         accuracy_fn: torchmetrics.Accuracy,
         bar: tqdm,
     ):
-        """Main test loop for one epoch
+        """Main test loop for one epoch.
 
         Args:
-            model (torch.nn.Module): model for testing
-            test_dataloader (torch.utils.data.DataLoader): data that will be fed to the model
-            loss_fn (torch.nn.Module): loss function
-            accuracy_fn (torchmetrics.Accuracy): accuracy function
-            bar (tqdm): tqdm object for printing progress
+            model (torch.nn.Module): model for testing.
+            test_dataloader (torch.utils.data.DataLoader): data that will be fed to the model.
+            loss_fn (torch.nn.Module): loss function.
+            accuracy_fn (torchmetrics.Accuracy): accuracy function.
+            bar (tqdm): tqdm object for printing progress.
 
         Returns:
-            tuple(float, float): tuple of test loss and test accuracy
+            tuple(float, float): tuple of test loss and test accuracy.
         """
 
         test_loss, test_acc = 0, 0
@@ -207,19 +243,20 @@ class Trainer:
 
         return test_loss, test_acc
 
-    def plot_confusion_matrix(self, model: torch.nn.Module, project_path: str):
+    def plot_confusion_matrix(self, model: torch.nn.Module, project_path: str, dataloader: torch.utils.data.DataLoader):
         """Method plots and saves confusion matrix and normalized confusion matrix.
 
         Args:
             model (torch.nn.Module): model to test predictions.
             project_path (str): path to where matrixes will be saved.
+            dataloader (torch.utils.data.DataLoader): val or test dataloader.
         """
 
         y_preds = []
         model.eval()
 
         with torch.inference_mode():
-            for x, y in self.test_dataloader:
+            for x, y in dataloader:
                 x, y = x.to(self.device), y.to(self.device)
 
                 y_pred = torch.softmax(model(x), dim=1).argmax(dim=1)
